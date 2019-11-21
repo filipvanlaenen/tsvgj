25237dbd91263ad23ff7ab56816d1a91190fe76c
Filip van Laenen <f.a.vanlaenen@ieee.org>
2019-11-21 13:30:15 +0100

    Issue #7: Resolved Checkstyle violations in the ArcToCommand class

--- src/main/java/net/filipvanlaenen/tsvgj/internal/ArcToCommand.java
+++ src/main/java/net/filipvanlaenen/tsvgj/internal/ArcToCommand.java
@@ -3,16 +3,60 @@ package net.filipvanlaenen.tsvgj.internal;
 import net.filipvanlaenen.tsvgj.Path.LargeArcFlagValues;
 import net.filipvanlaenen.tsvgj.Path.SweepFlagValues;
 
+/**
+ * A class representing an absolute arc-to command.
+ */
 public class ArcToCommand implements PathCommand {
 
+    /**
+     * The x coordinate for the center of the arc.
+     */
     private final Number cx;
+    /**
+     * The y coordinate for the center of the arc.
+     */
     private final Number cy;
+    /**
+     * The flag for whether to draw a large or a small arc.
+     */
     private final LargeArcFlagValues largeArcFlag;
+    /**
+     * The x radius of the arc.
+     */
     private final Number rx;
+    /**
+     * The y radius of the arc.
+     */
     private final Number ry;
+    /**
+     * The flag for whether to use the positive or the negative direction for the
+     * arc.
+     */
     private final SweepFlagValues sweepFlag;
+    /**
+     * The x axis rotation of the arc.
+     */
     private final Number xAxisRotation;
 
+    /**
+     * Constructs an absolute arc-to command.
+     *
+     * @param rx
+     *            The x radius of the arc.
+     * @param ry
+     *            The y radius of the arc.
+     * @param xAxisRotation
+     *            The x axis rotation of the arc.
+     * @param largeArcFlag
+     *            The flag for whether to draw a large or a small arc.
+     * @param sweepFlag
+     *            The flag for whether to use the positive or the negative direction
+     *            for the arc.
+     * @param cx
+     *            The x coordinate for the center of the arc.
+     * @param cy
+     *            The y coordinate for the center of the arc.
+     */
     public ArcToCommand(final Number rx, final Number ry, final Number xAxisRotation,
             final LargeArcFlagValues largeArcFlag, final SweepFlagValues sweepFlag, final Number cx, final Number cy) {
         this.rx = rx;
@@ -24,6 +68,11 @@ public class ArcToCommand implements PathCommand {
         this.cy = cy;
     }
 
+    /**
+     * Returns a string representation of the absolute arc-to command.
+     *
+     * @return A string representation of the absolute arc-to command.
+     */
     @Override
     public String asString() {
         return "A " + Attribute.DECIMAL_FORMAT.format(rx) + " " + Attribute.DECIMAL_FORMAT.format(ry) + " "
