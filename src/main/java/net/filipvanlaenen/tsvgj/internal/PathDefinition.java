f00343933d98f662169275a1868d46851b55c4c8
Filip van Laenen <f.a.vanlaenen@ieee.org>
2019-11-21 13:20:35 +0100

    Issue #7: Resolved Checkstyle violations in the PathDefinition class

--- src/main/java/net/filipvanlaenen/tsvgj/internal/PathDefinition.java
+++ src/main/java/net/filipvanlaenen/tsvgj/internal/PathDefinition.java
@@ -4,14 +4,28 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
+/**
+ * A class representing the definition of a {@link Path}.
+ */
 public class PathDefinition implements Attribute {
-
+    /**
+     * The list with commands.
+     */
     private final List<PathCommand> commands = new ArrayList<PathCommand>();
 
+    /**
+     * Adds a command to the path definition.
+     *
+     * @param command
+     *            The command to be added to the path definition.
+     */
     public void add(final PathCommand command) {
         commands.add(command);
     }
 
+    /**
+     * Returns a string representation of the path definition.
+     */
     @Override
     public String asString() {
         List<String> commandStrings = new ArrayList<String>();
@@ -25,7 +39,6 @@ public class PathDefinition implements Attribute {
         } else {
             return String.join(" ", commandStrings);
         }
-
     }
 
 }
