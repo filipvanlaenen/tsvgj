bbdb4433b8c884e8e8af591558f738da140a7d3e
Filip van Laenen <f.a.vanlaenen@ieee.org>
2019-11-21 13:26:44 +0100

    Issue #7: Resolved Checkstyle violations in the LineToCommand and MoveToCommand class

--- src/main/java/net/filipvanlaenen/tsvgj/internal/LineToCommand.java
+++ src/main/java/net/filipvanlaenen/tsvgj/internal/LineToCommand.java
@@ -1,15 +1,36 @@
 package net.filipvanlaenen.tsvgj.internal;
 
+/**
+ * A class representing an absolute line-to command.
+ */
 public class LineToCommand implements PathCommand {
-
+    /**
+     * The x coordinate to draw a line to.
+     */
     private final Number x;
+    /**
+     * The y coordinate to draw a line to.
+     */
     private final Number y;
 
+    /**
+     * Constructs an absolute line-to command.
+     *
+     * @param x
+     *            The x coordinate to draw a line to.
+     * @param y
+     *            The y coordinate to draw a line to.
+     */
     public LineToCommand(final Number x, final Number y) {
         this.x = x;
         this.y = y;
     }
 
+    /**
+     * Returns a string representation of the absolute line-to command.
+     *
+     * @return A string representation of the absolute line-to command.
+     */
     @Override
     public String asString() {
         return "L " + Attribute.DECIMAL_FORMAT.format(x) + " " + Attribute.DECIMAL_FORMAT.format(y);
--- src/main/java/net/filipvanlaenen/tsvgj/internal/MoveToCommand.java
+++ src/main/java/net/filipvanlaenen/tsvgj/internal/MoveToCommand.java
@@ -1,14 +1,36 @@
 package net.filipvanlaenen.tsvgj.internal;
 
+/**
+ * A class representing an absolute move-to command.
+ */
 public class MoveToCommand implements PathCommand {
+    /**
+     * The x coordinate to move to.
+     */
     private final Number x;
+    /**
+     * The y coordinate to move to.
+     */
     private final Number y;
 
+    /**
+     * Constructs an absolute move-to command.
+     *
+     * @param x
+     *            The x coordinate to move to.
+     * @param y
+     *            The y coordinate to move to.
+     */
     public MoveToCommand(final Number x, final Number y) {
         this.x = x;
         this.y = y;
     }
 
+    /**
+     * Returns a string representation of the absolute move-to command.
+     *
+     * @return A string representation of the absolute move-to command.
+     */
     @Override
     public String asString() {
         return "M " + Attribute.DECIMAL_FORMAT.format(x) + " " + Attribute.DECIMAL_FORMAT.format(y);
