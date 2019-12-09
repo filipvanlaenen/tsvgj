036578b45d97207059832bc4bd6cffcdd50672c8
Filip van Laenen <f.a.vanlaenen@ieee.org>
2019-12-09 08:00:44 +0100

    Issue #13: Resolved Checkstyle violations

--- src/main/java/net/filipvanlaenen/tsvgj/ColorKeyword.java
+++ src/main/java/net/filipvanlaenen/tsvgj/ColorKeyword.java
@@ -1,6 +1,592 @@
 package net.filipvanlaenen.tsvgj;
 
 public enum ColorKeyword {
-    ALICEBLUE, ANTIQUEWHITE, AQUA, AQUAMARINE, AZURE, BEIGE, BISQUE, BLACK, BLANCHEDALMOND, BLUE, BLUEVIOLET, BROWN, BURLYWOOD, CADETBLUE, CHARTREUSE, CHOCOLATE, CORAL, CORNFLOWERBLUE, CORNSILK, CRIMSON, CYAN, DARKBLUE, DARKCYAN, DARKGOLDENROD, DARKGRAY, DARKGREEN, DARKGREY, DARKKHAKI, DARKMAGENTA, DARKOLIVEGREEN, DARKORANGE, DARKORCHID, DARKRED, DARKSALMON, DARKSEAGREEN, DARKSLATEBLUE, DARKSLATEGRAY, DARKSLATEGREY, DARKTURQUOISE, DARKVIOLET, DEEPPINK, DEEPSKYBLUE, DIMGRAY, DIMGREY, DODGERBLUE, FIREBRICK, FLORALWHITE, FORESTGREEN, FUCHSIA, GAINSBORO, GHOSTWHITE, GOLD, GOLDENROD, GRAY, GREY, GREEN, GREENYELLOW, HONEYDEW, HOTPINK, INDIANRED, INDIGO, IVORY, KHAKI, LAVENDER, LAVENDERBLUSH, LAWNGREEN, LEMONCHIFFON, LIGHTBLUE, LIGHTCORAL, LIGHTCYAN, LIGHTGOLDENRODYELLOW, LIGHTGRAY, LIGHTGREEN, LIGHTGREY, LIGHTPINK, LIGHTSALMON, LIGHTSEAGREEN, LIGHTSKYBLUE, LIGHTSLATEGRAY, LIGHTSLATEGREY, LIGHTSTEELBLUE, LIGHTYELLOW, LIME, LIMEGREEN, LINEN, MAGENTA, MAROON, MEDIUMAQUAMARINE, MEDIUMBLUE, MEDIUMORCHID, MEDIUMPURPLE, MEDIUMSEAGREEN, MEDIUMSLATEBLUE, MEDIUMSPRINGGREEN, MEDIUMTURQUOISE, MEDIUMVIOLETRED, MIDNIGHTBLUE, MINTCREAM, MISTYROSE, MOCCASIN, NAVAJOWHITE, NAVY, OLDLACE, OLIVE, OLIVEDRAB, ORANGE, ORANGERED, ORCHID, PALEGOLDENROD, PALEGREEN, PALETURQUOISE, PALEVIOLETRED, PAPAYAWHIP, PEACHPUFF, PERU, PINK, PLUM, POWDERBLUE, PURPLE, RED, ROSYBROWN, ROYALBLUE, SADDLEBROWN, SALMON, SANDYBROWN, SEAGREEN, SEASHELL, SIENNA, SILVER, SKYBLUE, SLATEBLUE, SLATEGRAY, SLATEGREY, SNOW, SPRINGGREEN, STEELBLUE, TAN, TEAL, THISTLE, TOMATO, TURQUOISE, VIOLET, WHEAT, WHITE, WHITESMOKE, YELLOW, YELLOWGREEN
-
+    /**
+     * SVG color keyword for aliceblue.
+     */
+    ALICEBLUE,
+    /**
+     * SVG color keyword for antiquewhite.
+     */
+    ANTIQUEWHITE,
+    /**
+     * SVG color keyword for aqua.
+     */
+    AQUA,
+    /**
+     * SVG color keyword for aquamarine.
+     */
+    AQUAMARINE,
+    /**
+     * SVG color keyword for azure.
+     */
+    AZURE,
+    /**
+     * SVG color keyword for beige.
+     */
+    BEIGE,
+    /**
+     * SVG color keyword for bisque.
+     */
+    BISQUE,
+    /**
+     * SVG color keyword for black.
+     */
+    BLACK,
+    /**
+     * SVG color keyword for blanchedalmond.
+     */
+    BLANCHEDALMOND,
+    /**
+     * SVG color keyword for blue.
+     */
+    BLUE,
+    /**
+     * SVG color keyword for blueviolet.
+     */
+    BLUEVIOLET,
+    /**
+     * SVG color keyword for brown.
+     */
+    BROWN,
+    /**
+     * SVG color keyword for burlywood.
+     */
+    BURLYWOOD,
+    /**
+     * SVG color keyword for cadetblue.
+     */
+    CADETBLUE,
+    /**
+     * SVG color keyword for chartreuse.
+     */
+    CHARTREUSE,
+    /**
+     * SVG color keyword for chocolate.
+     */
+    CHOCOLATE,
+    /**
+     * SVG color keyword for coral.
+     */
+    CORAL,
+    /**
+     * SVG color keyword for cornflowerblue.
+     */
+    CORNFLOWERBLUE,
+    /**
+     * SVG color keyword for cornsilk.
+     */
+    CORNSILK,
+    /**
+     * SVG color keyword for crimson.
+     */
+    CRIMSON,
+    /**
+     * SVG color keyword for cyan.
+     */
+    CYAN,
+    /**
+     * SVG color keyword for darkblue.
+     */
+    DARKBLUE,
+    /**
+     * SVG color keyword for darkcyan.
+     */
+    DARKCYAN,
+    /**
+     * SVG color keyword for darkgoldenrod.
+     */
+    DARKGOLDENROD,
+    /**
+     * SVG color keyword for darkgray.
+     */
+    DARKGRAY,
+    /**
+     * SVG color keyword for darkgreen.
+     */
+    DARKGREEN,
+    /**
+     * SVG color keyword for darkgrey.
+     */
+    DARKGREY,
+    /**
+     * SVG color keyword for darkkhaki.
+     */
+    DARKKHAKI,
+    /**
+     * SVG color keyword for darkmagenta.
+     */
+    DARKMAGENTA,
+    /**
+     * SVG color keyword for darkolivegreen.
+     */
+    DARKOLIVEGREEN,
+    /**
+     * SVG color keyword for darkorange.
+     */
+    DARKORANGE,
+    /**
+     * SVG color keyword for darkorchid.
+     */
+    DARKORCHID,
+    /**
+     * SVG color keyword for darkred.
+     */
+    DARKRED,
+    /**
+     * SVG color keyword for darksalmon.
+     */
+    DARKSALMON,
+    /**
+     * SVG color keyword for darkseagreen.
+     */
+    DARKSEAGREEN,
+    /**
+     * SVG color keyword for darkslateblue.
+     */
+    DARKSLATEBLUE,
+    /**
+     * SVG color keyword for darkslategray.
+     */
+    DARKSLATEGRAY,
+    /**
+     * SVG color keyword for darkslategrey.
+     */
+    DARKSLATEGREY,
+    /**
+     * SVG color keyword for darkturquoise.
+     */
+    DARKTURQUOISE,
+    /**
+     * SVG color keyword for darkviolet.
+     */
+    DARKVIOLET,
+    /**
+     * SVG color keyword for deeppink.
+     */
+    DEEPPINK,
+    /**
+     * SVG color keyword for deepskyblue.
+     */
+    DEEPSKYBLUE,
+    /**
+     * SVG color keyword for dimgray.
+     */
+    DIMGRAY,
+    /**
+     * SVG color keyword for dimgrey.
+     */
+    DIMGREY,
+    /**
+     * SVG color keyword for dodgerblue.
+     */
+    DODGERBLUE,
+    /**
+     * SVG color keyword for firebrick.
+     */
+    FIREBRICK,
+    /**
+     * SVG color keyword for floralwhite.
+     */
+    FLORALWHITE,
+    /**
+     * SVG color keyword for forestgreen.
+     */
+    FORESTGREEN,
+    /**
+     * SVG color keyword for fuchsia.
+     */
+    FUCHSIA,
+    /**
+     * SVG color keyword for gainsboro.
+     */
+    GAINSBORO,
+    /**
+     * SVG color keyword for ghostwhite.
+     */
+    GHOSTWHITE,
+    /**
+     * SVG color keyword for gold.
+     */
+    GOLD,
+    /**
+     * SVG color keyword for goldenrod.
+     */
+    GOLDENROD,
+    /**
+     * SVG color keyword for gray.
+     */
+    GRAY,
+    /**
+     * SVG color keyword for grey.
+     */
+    GREY,
+    /**
+     * SVG color keyword for green.
+     */
+    GREEN,
+    /**
+     * SVG color keyword for greenyellow.
+     */
+    GREENYELLOW,
+    /**
+     * SVG color keyword for honeydew.
+     */
+    HONEYDEW,
+    /**
+     * SVG color keyword for hotpink.
+     */
+    HOTPINK,
+    /**
+     * SVG color keyword for indianred.
+     */
+    INDIANRED,
+    /**
+     * SVG color keyword for indigo.
+     */
+    INDIGO,
+    /**
+     * SVG color keyword for ivory.
+     */
+    IVORY,
+    /**
+     * SVG color keyword for khaki.
+     */
+    KHAKI,
+    /**
+     * SVG color keyword for lavender.
+     */
+    LAVENDER,
+    /**
+     * SVG color keyword for lavenderblush.
+     */
+    LAVENDERBLUSH,
+    /**
+     * SVG color keyword for lawngreen.
+     */
+    LAWNGREEN,
+    /**
+     * SVG color keyword for lemonchiffon.
+     */
+    LEMONCHIFFON,
+    /**
+     * SVG color keyword for lightblue.
+     */
+    LIGHTBLUE,
+    /**
+     * SVG color keyword for lightcoral.
+     */
+    LIGHTCORAL,
+    /**
+     * SVG color keyword for lightcyan.
+     */
+    LIGHTCYAN,
+    /**
+     * SVG color keyword for lightgoldenrodyellow.
+     */
+    LIGHTGOLDENRODYELLOW,
+    /**
+     * SVG color keyword for lightgray.
+     */
+    LIGHTGRAY,
+    /**
+     * SVG color keyword for lightgreen.
+     */
+    LIGHTGREEN,
+    /**
+     * SVG color keyword for lightgrey.
+     */
+    LIGHTGREY,
+    /**
+     * SVG color keyword for lightpink.
+     */
+    LIGHTPINK,
+    /**
+     * SVG color keyword for lightsalmon.
+     */
+    LIGHTSALMON,
+    /**
+     * SVG color keyword for lightseagreen.
+     */
+    LIGHTSEAGREEN,
+    /**
+     * SVG color keyword for lightskyblue.
+     */
+    LIGHTSKYBLUE,
+    /**
+     * SVG color keyword for lightslategray.
+     */
+    LIGHTSLATEGRAY,
+    /**
+     * SVG color keyword for lightslategrey.
+     */
+    LIGHTSLATEGREY,
+    /**
+     * SVG color keyword for lightsteelblue.
+     */
+    LIGHTSTEELBLUE,
+    /**
+     * SVG color keyword for lightyellow.
+     */
+    LIGHTYELLOW,
+    /**
+     * SVG color keyword for lime.
+     */
+    LIME,
+    /**
+     * SVG color keyword for limegreen.
+     */
+    LIMEGREEN,
+    /**
+     * SVG color keyword for linen.
+     */
+    LINEN,
+    /**
+     * SVG color keyword for magenta.
+     */
+    MAGENTA,
+    /**
+     * SVG color keyword for maroon.
+     */
+    MAROON,
+    /**
+     * SVG color keyword for mediumaquamarine.
+     */
+    MEDIUMAQUAMARINE,
+    /**
+     * SVG color keyword for mediumblue.
+     */
+    MEDIUMBLUE,
+    /**
+     * SVG color keyword for mediumorchid.
+     */
+    MEDIUMORCHID,
+    /**
+     * SVG color keyword for mediumpurple.
+     */
+    MEDIUMPURPLE,
+    /**
+     * SVG color keyword for mediumseagreen.
+     */
+    MEDIUMSEAGREEN,
+    /**
+     * SVG color keyword for mediumslateblue.
+     */
+    MEDIUMSLATEBLUE,
+    /**
+     * SVG color keyword for mediumspringgreen.
+     */
+    MEDIUMSPRINGGREEN,
+    /**
+     * SVG color keyword for mediumturquoise.
+     */
+    MEDIUMTURQUOISE,
+    /**
+     * SVG color keyword for mediumvioletred.
+     */
+    MEDIUMVIOLETRED,
+    /**
+     * SVG color keyword for midnightblue.
+     */
+    MIDNIGHTBLUE,
+    /**
+     * SVG color keyword for mintcream.
+     */
+    MINTCREAM,
+    /**
+     * SVG color keyword for mistyrose.
+     */
+    MISTYROSE,
+    /**
+     * SVG color keyword for moccasin.
+     */
+    MOCCASIN,
+    /**
+     * SVG color keyword for navajowhite.
+     */
+    NAVAJOWHITE,
+    /**
+     * SVG color keyword for navy.
+     */
+    NAVY,
+    /**
+     * SVG color keyword for oldlace.
+     */
+    OLDLACE,
+    /**
+     * SVG color keyword for olive.
+     */
+    OLIVE,
+    /**
+     * SVG color keyword for olivedrab.
+     */
+    OLIVEDRAB,
+    /**
+     * SVG color keyword for orange.
+     */
+    ORANGE,
+    /**
+     * SVG color keyword for orangered.
+     */
+    ORANGERED,
+    /**
+     * SVG color keyword for orchid.
+     */
+    ORCHID,
+    /**
+     * SVG color keyword for palegoldenrod.
+     */
+    PALEGOLDENROD,
+    /**
+     * SVG color keyword for palegreen.
+     */
+    PALEGREEN,
+    /**
+     * SVG color keyword for paleturquoise.
+     */
+    PALETURQUOISE,
+    /**
+     * SVG color keyword for palevioletred.
+     */
+    PALEVIOLETRED,
+    /**
+     * SVG color keyword for papayawhip.
+     */
+    PAPAYAWHIP,
+    /**
+     * SVG color keyword for peachpuff.
+     */
+    PEACHPUFF,
+    /**
+     * SVG color keyword for peru.
+     */
+    PERU,
+    /**
+     * SVG color keyword for pink.
+     */
+    PINK,
+    /**
+     * SVG color keyword for plum.
+     */
+    PLUM,
+    /**
+     * SVG color keyword for powderblue.
+     */
+    POWDERBLUE,
+    /**
+     * SVG color keyword for purple.
+     */
+    PURPLE,
+    /**
+     * SVG color keyword for red.
+     */
+    RED,
+    /**
+     * SVG color keyword for rosybrown.
+     */
+    ROSYBROWN,
+    /**
+     * SVG color keyword for royalblue.
+     */
+    ROYALBLUE,
+    /**
+     * SVG color keyword for saddlebrown.
+     */
+    SADDLEBROWN,
+    /**
+     * SVG color keyword for salmon.
+     */
+    SALMON,
+    /**
+     * SVG color keyword for sandybrown.
+     */
+    SANDYBROWN,
+    /**
+     * SVG color keyword for seagreen.
+     */
+    SEAGREEN,
+    /**
+     * SVG color keyword for seashell.
+     */
+    SEASHELL,
+    /**
+     * SVG color keyword for sienna.
+     */
+    SIENNA,
+    /**
+     * SVG color keyword for silver.
+     */
+    SILVER,
+    /**
+     * SVG color keyword for skyblue.
+     */
+    SKYBLUE,
+    /**
+     * SVG color keyword for slateblue.
+     */
+    SLATEBLUE,
+    /**
+     * SVG color keyword for slategray.
+     */
+    SLATEGRAY,
+    /**
+     * SVG color keyword for slategrey.
+     */
+    SLATEGREY,
+    /**
+     * SVG color keyword for snow.
+     */
+    SNOW,
+    /**
+     * SVG color keyword for springgreen.
+     */
+    SPRINGGREEN,
+    /**
+     * SVG color keyword for steelblue.
+     */
+    STEELBLUE,
+    /**
+     * SVG color keyword for tan.
+     */
+    TAN,
+    /**
+     * SVG color keyword for teal.
+     */
+    TEAL,
+    /**
+     * SVG color keyword for thistle.
+     */
+    THISTLE,
+    /**
+     * SVG color keyword for tomato.
+     */
+    TOMATO,
+    /**
+     * SVG color keyword for turquoise.
+     */
+    TURQUOISE,
+    /**
+     * SVG color keyword for violet.
+     */
+    VIOLET,
+    /**
+     * SVG color keyword for wheat.
+     */
+    WHEAT,
+    /**
+     * SVG color keyword for white.
+     */
+    WHITE,
+    /**
+     * SVG color keyword for whitesmoke.
+     */
+    WHITESMOKE,
+    /**
+     * SVG color keyword for yellow.
+     */
+    YELLOW,
+    /**
+     * SVG color keyword for yellowgreen.
+     */
+    YELLOWGREEN
 }
