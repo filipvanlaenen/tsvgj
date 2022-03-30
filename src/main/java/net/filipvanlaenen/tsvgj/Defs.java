de7de1cb3be41c7f60eac28ba172cde3359e72cd
Filip van Laenen <f.a.vanlaenen@ieee.org>
2022-03-30 12:23:33 +0200

    Issue #17: Refactored Defs to use BLTXMLPEJ

--- src/main/java/net/filipvanlaenen/tsvgj/Defs.java
+++ src/main/java/net/filipvanlaenen/tsvgj/Defs.java
@@ -1,6 +1,6 @@
 package net.filipvanlaenen.tsvgj;
 
-import net.filipvanlaenen.tsvgj.internal.DeprecatedElements;
+import net.filipvanlaenen.bltxmlepj.ElementWithElements;
 
 /**
  * A class representing definitions.
@@ -8,44 +8,21 @@ import net.filipvanlaenen.tsvgj.internal.DeprecatedElements;
  * @see <a href="https://www.w3.org/TR/SVG/struct.html#DefsElement">Document
  *      Structure — SVG 2: 5.4.2. The ‘defs’ element</a>
  */
-public class Defs implements StructuralElement {
+public final class Defs extends ElementWithElements implements StructuralElement {
     /**
-     * A list with the elements.
+     * Counter to assign IDs to paint server elements.
      */
-    private final DeprecatedElements elements = new DeprecatedElements();
+    private int paintServerElementIdCounter = 0;
 
     /**
      * Adds a paint server element.
      *
-     * @param paintServerElement A paint server element.
+     * @param paintServerElement
+     *            A paint server element.
      */
     public void addElement(final PaintServerElement paintServerElement) {
-        elements.add(paintServerElement);
-        paintServerElement.setId(elements.size());
-    }
-
-    /**
-     * Returns a string representation of the definitions with the provided
-     * indentation.
-     *
-     * @param indent The indentation.
-     * @return A string representation of the definitions with the provided
-     *         indentation.
-     */
-    @Override
-    public String asString(final String indent) {
-        StringBuilder sb = new StringBuilder();
-        sb.append(indent);
-        sb.append("<defs");
-        if (elements.isEmpty()) {
-            sb.append("/>");
-        } else {
-            sb.append(">\n");
-            sb.append(elements.asString(indent + "  "));
-            sb.append(indent);
-            sb.append("</defs>");
-        }
-        return sb.toString();
+        super.addElement(paintServerElement);
+        paintServerElement.setId(++paintServerElementIdCounter);
     }
 
     @Override
