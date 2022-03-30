142ec823f1db52d0df9b1993c363542213e221fc
Filip van Laenen <f.a.vanlaenen@ieee.org>
2022-03-30 16:00:15 +0200

    Resolved some Checkstyle violations

--- src/main/java/net/filipvanlaenen/tsvgj/internal/HexadecimalColorAttribute.java
+++ src/main/java/net/filipvanlaenen/tsvgj/internal/HexadecimalColorAttribute.java
@@ -2,12 +2,21 @@ package net.filipvanlaenen.tsvgj.internal;
 
 import net.filipvanlaenen.bltxmlepj.Attribute;
 
-public class HexadecimalColorAttribute implements Attribute {
+/**
+ * A class representing a color attribute, with the code provided as an integer.
+ */
+public final class HexadecimalColorAttribute implements Attribute {
     /**
      * The color as an integer.
      */
     private final Integer color;
 
+    /**
+     * Constructor taking the color as an integer as a parameter.
+     *
+     * @param color
+     *            The color as an integer.
+     */
     public HexadecimalColorAttribute(final Integer color) {
         this.color = color;
     }
--- src/main/java/net/filipvanlaenen/tsvgj/internal/KeywordColorAttribute.java
+++ src/main/java/net/filipvanlaenen/tsvgj/internal/KeywordColorAttribute.java
@@ -3,12 +3,21 @@ package net.filipvanlaenen.tsvgj.internal;
 import net.filipvanlaenen.bltxmlepj.Attribute;
 import net.filipvanlaenen.tsvgj.ColorKeyword;
 
-public class KeywordColorAttribute implements Attribute {
+/**
+ * A class representing a color attribute using a keyword.
+ */
+public final class KeywordColorAttribute implements Attribute {
     /**
      * The color keyword.
      */
     private final ColorKeyword color;
 
+    /**
+     * Constructor taking the color as a keyword as its parameter.
+     *
+     * @param color
+     *            The color as a keyword.
+     */
     public KeywordColorAttribute(final ColorKeyword color) {
         this.color = color;
     }
