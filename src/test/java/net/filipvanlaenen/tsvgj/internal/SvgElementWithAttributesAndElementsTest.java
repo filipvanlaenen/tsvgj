e3edccaa441b2cbe53669b116a7ee86761ef98ed
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2022-08-09 18:50:06 +0200

    Issue #25: Added unit test to SvgElementWithAttributesAndElements

--- src/test/java/net/filipvanlaenen/tsvgj/internal/SvgElementWithAttributesAndElementsTest.java
+++ src/test/java/net/filipvanlaenen/tsvgj/internal/SvgElementWithAttributesAndElementsTest.java
@@ -4,6 +4,8 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
+import net.filipvanlaenen.bltxmlepj.AttributeValueEnumeration;
+
 /**
  * Unit tests on the <code>SvgElementWithAttributesAndElements</code> class.
  */
@@ -14,14 +16,27 @@ public class SvgElementWithAttributesAndElementsTest {
     private static final int THREE = 3;
 
     /**
-     * Local subclass of <code>ElementWithAttributesAndElements</code> for testing
-     * purposes.
+     * Local subclass of <code>ElementWithAttributesAndElements</code> for testing purposes.
      */
     private class MySvgElementWithAttributesAndElements extends SvgElementWithAttributesAndElements {
         @Override
         public String getElementName() {
             return "e";
         }
+    }
+
+    /**
+     * Local enumeration for testing purposes.
+     */
+    private enum MyEnumeration implements AttributeValueEnumeration {
+        /**
+         * Simple value.
+         */
+        FOO,
+        /**
+         * Another simple value.
+         */
+        BAR
     }
 
     /**
@@ -32,5 +47,15 @@ public class SvgElementWithAttributesAndElementsTest {
         MySvgElementWithAttributesAndElements element = new MySvgElementWithAttributesAndElements();
         element.addNumericArrayAttribute("foo", 1, 2, THREE);
         assertEquals("<e foo=\"1 2 3\"/>", element.asString());
+    }
+
+    /**
+     * Tests that an element with an enumeration array attribute is exported correctly.
+     */
+    @Test
+    void elementWithEnumerationArrayAttributeIsExportedCorrectly() {
+        MySvgElementWithAttributesAndElements element = new MySvgElementWithAttributesAndElements();
+        element.addEnumerationArrayAttribute("baz", MyEnumeration.FOO, MyEnumeration.BAR);
+        assertEquals("<e baz=\"foo bar\"/>", element.asString());
     }
 }
