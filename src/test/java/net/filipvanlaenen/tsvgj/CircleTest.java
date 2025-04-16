1d8df9e88b6bfaecbf0cd8f5e145cabeaf8dc5b8
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2025-04-16 09:50:36 +0200

    Issue #67: Added unit tests to CircleTest

--- src/test/java/net/filipvanlaenen/tsvgj/CircleTest.java
+++ src/test/java/net/filipvanlaenen/tsvgj/CircleTest.java
@@ -18,8 +18,7 @@ public class CircleTest {
     private static final double HALF = 0.5D;
 
     /**
-     * Test verifying that a circle with a color specified as a hexadecimal integer
-     * is exported correctly to a string.
+     * Test verifying that a circle with a color specified as a hexadecimal integer is exported correctly to a string.
      */
     @Test
     void circleWithHexadecimalColorIsConvertedCorrectlyToString() {
@@ -30,8 +29,7 @@ public class CircleTest {
     }
 
     /**
-     * Test verifying that a circle with a color specified using a color keyword is
-     * exported correctly to a string.
+     * Test verifying that a circle with a color specified using a color keyword is exported correctly to a string.
      */
     @Test
     void circleWithKeywordColorIsConvertedCorrectlyToString() {
@@ -42,8 +40,7 @@ public class CircleTest {
     }
 
     /**
-     * Test verifying that a red circle with opacity 0.5 is exported correctly to a
-     * string.
+     * Test verifying that a red circle with opacity 0.5 is exported correctly to a string.
      */
     @Test
     void redCircleHalfOpaqueIsConvertedCorrectlyToString() {
@@ -54,8 +51,7 @@ public class CircleTest {
     }
 
     /**
-     * Test verifying that a red circle with fill opacity 0.5 is exported correctly
-     * to a string.
+     * Test verifying that a red circle with fill opacity 0.5 is exported correctly to a string.
      */
     @Test
     void redCircleFillHalfOpaqueIsConvertedCorrectlyToString() {
@@ -66,8 +62,7 @@ public class CircleTest {
     }
 
     /**
-     * Test verifying that a red circle with a stroke and stroke width but no fill
-     * is exported correctly to a string.
+     * Test verifying that a red circle with a stroke and stroke width but no fill is exported correctly to a string.
      */
     @Test
     void redOutlinedCircleIsConvertedCorrectlyToString() {
@@ -78,14 +73,66 @@ public class CircleTest {
     }
 
     /**
-     * Test verifying that a circle with a hexadecimal stroke color and a stroke
-     * width is exported correctly to a string.
+     * Test verifying that a circle with a hexadecimal stroke color and a stroke width is exported correctly to a
+     * string.
      */
     @Test
     void hexadecimalColorOutlinedCircleIsConvertedCorrectlyToString() {
         Circle circle = new Circle().cx(0).cy(1).r(2).stroke(RED).strokeWidth(HALF);
         String actual = circle.asString();
         String expected = "<circle cx=\"0\" cy=\"1\" r=\"2\" stroke=\"#FF0000\" stroke-width=\"0.5\"/>";
+        assertEquals(expected, actual);
+    }
+
+    /**
+     * Test verifying that a circle with an end marker is exported correctly.
+     */
+    @Test
+    void circleWithEndMarkerShouldBeConvertedCorrectlyToString() {
+        Marker marker = new Marker();
+        marker.setId(1);
+        Circle circle = new Circle().cx(0).cy(1).r(2).markerEnd(marker);
+        String actual = circle.asString();
+        String expected = "<circle cx=\"0\" cy=\"1\" marker-end=\"url(#marker-1)\" r=\"2\"/>";
+        assertEquals(expected, actual);
+    }
+
+    /**
+     * Test verifying that a circle with an middle marker is exported correctly.
+     */
+    @Test
+    void circleWithMiddleMarkerShouldBeConvertedCorrectlyToString() {
+        Marker marker = new Marker();
+        marker.setId(1);
+        Circle circle = new Circle().cx(0).cy(1).r(2).markerMid(marker);
+        String actual = circle.asString();
+        String expected = "<circle cx=\"0\" cy=\"1\" marker-mid=\"url(#marker-1)\" r=\"2\"/>";
+        assertEquals(expected, actual);
+    }
+
+    /**
+     * Test verifying that a circle with an start marker is exported correctly.
+     */
+    @Test
+    void circleWithStartMarkerShouldBeConvertedCorrectlyToString() {
+        Marker marker = new Marker();
+        marker.setId(1);
+        Circle circle = new Circle().cx(0).cy(1).r(2).markerStart(marker);
+        String actual = circle.asString();
+        String expected = "<circle cx=\"0\" cy=\"1\" marker-start=\"url(#marker-1)\" r=\"2\"/>";
+        assertEquals(expected, actual);
+    }
+
+    /**
+     * Test verifying that a circle with a mask is exported correctly.
+     */
+    @Test
+    void circleWithMaskShouldBeConvertedCorrectlyToString() {
+        Mask mask = new Mask();
+        mask.setId(1);
+        Circle circle = new Circle().cx(0).cy(1).r(2).mask(mask);
+        String actual = circle.asString();
+        String expected = "<circle cx=\"0\" cy=\"1\" mask=\"url(#mask-1)\" r=\"2\"/>";
         assertEquals(expected, actual);
     }
 
