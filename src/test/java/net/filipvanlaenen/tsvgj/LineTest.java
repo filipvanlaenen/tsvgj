be3006c6b478776091f81873326c52dbb6bc6852
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2025-04-16 12:13:30 +0200

    Issue #67: Added unit tests to LineTest

--- src/test/java/net/filipvanlaenen/tsvgj/LineTest.java
+++ src/test/java/net/filipvanlaenen/tsvgj/LineTest.java
@@ -22,8 +22,7 @@ public class LineTest {
     private static final int FOUR = 4;
 
     /**
-     * Test verifying that a line with a hexadecimal color is exported correctly to
-     * a string.
+     * Test verifying that a line with a hexadecimal color is exported correctly to a string.
      */
     @Test
     void lineWithHexadecimalColorStrokeIsConvertedCorrectlyToString() {
@@ -34,8 +33,7 @@ public class LineTest {
     }
 
     /**
-     * Test verifying that a line with a keyword color is exported correctly to a
-     * string.
+     * Test verifying that a line with a keyword color is exported correctly to a string.
      */
     @Test
     void lineWithKeywordColorStrokeIsConvertedCorrectlyToString() {
@@ -45,4 +43,63 @@ public class LineTest {
         assertEquals(expected, actual);
     }
 
+    /**
+     * Test verifying that a line with an end marker is exported correctly.
+     */
+    @Test
+    void lineWithEndMarkerShouldBeConvertedCorrectlyToString() {
+        Marker marker = new Marker();
+        marker.setId(1);
+        Line line = new Line().x1(0).y1(1).x2(2).y2(THREE).stroke(ColorKeyword.RED).strokeWidth(FOUR).markerEnd(marker);
+        String actual = line.asString();
+        String expected =
+                "<line marker-end=\"url(#marker-1)\" stroke=\"red\" stroke-width=\"4\" x1=\"0\" x2=\"2\" y1=\"1\""
+                        + " y2=\"3\"/>";
+        assertEquals(expected, actual);
+    }
+
+    /**
+     * Test verifying that a line with an middle marker is exported correctly.
+     */
+    @Test
+    void lineWithMiddleMarkerShouldBeConvertedCorrectlyToString() {
+        Marker marker = new Marker();
+        marker.setId(1);
+        Line line = new Line().x1(0).y1(1).x2(2).y2(THREE).stroke(ColorKeyword.RED).strokeWidth(FOUR).markerMid(marker);
+        String actual = line.asString();
+        String expected =
+                "<line marker-mid=\"url(#marker-1)\" stroke=\"red\" stroke-width=\"4\" x1=\"0\" x2=\"2\" y1=\"1\""
+                        + " y2=\"3\"/>";
+        assertEquals(expected, actual);
+    }
+
+    /**
+     * Test verifying that a line with an start marker is exported correctly.
+     */
+    @Test
+    void lineWithStartMarkerShouldBeConvertedCorrectlyToString() {
+        Marker marker = new Marker();
+        marker.setId(1);
+        Line line =
+                new Line().x1(0).y1(1).x2(2).y2(THREE).stroke(ColorKeyword.RED).strokeWidth(FOUR).markerStart(marker);
+        String actual = line.asString();
+        String expected =
+                "<line marker-start=\"url(#marker-1)\" stroke=\"red\" stroke-width=\"4\" x1=\"0\" x2=\"2\" y1=\"1\""
+                        + " y2=\"3\"/>";
+        assertEquals(expected, actual);
+    }
+
+    /**
+     * Test verifying that a line with a mask is exported correctly.
+     */
+    @Test
+    void lineWithMaskShouldBeConvertedCorrectlyToString() {
+        Mask mask = new Mask();
+        mask.setId(1);
+        Line line = new Line().x1(0).y1(1).x2(2).y2(THREE).stroke(ColorKeyword.RED).strokeWidth(FOUR).mask(mask);
+        String actual = line.asString();
+        String expected = "<line mask=\"url(#mask-1)\" stroke=\"red\" stroke-width=\"4\" x1=\"0\" x2=\"2\" y1=\"1\""
+                + " y2=\"3\"/>";
+        assertEquals(expected, actual);
+    }
 }
