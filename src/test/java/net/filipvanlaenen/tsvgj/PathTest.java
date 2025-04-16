0521174cf8d42e31bed0d246d3ca02b1440cdbb3
Filip Van Laenen <f.a.vanlaenen@ieee.org>
2025-04-16 12:18:14 +0200

    Issue #67: Added unit tests to PathTest

--- src/test/java/net/filipvanlaenen/tsvgj/PathTest.java
+++ src/test/java/net/filipvanlaenen/tsvgj/PathTest.java
@@ -42,8 +42,7 @@ public class PathTest {
     private static final int SEVEN = 7;
 
     /**
-     * Test verifying that a path describing a red line is exported correctly to a
-     * string.
+     * Test verifying that a path describing a red line is exported correctly to a string.
      */
     @Test
     void redLineIsConvertedCorrectlyToString() {
@@ -54,21 +53,22 @@ public class PathTest {
     }
 
     /**
-     * Test verifying that a path describing a red sector line is exported correctly
-     * to a string.
+     * Test verifying that a path describing a red sector line is exported correctly to a string.
      */
     @Test
     void redSectorLineIsConvertedCorrectlyToString() {
-        Path path = new Path().moveTo(0, 1).arcTo(THREE, FOUR, FIVE, Path.LargeArcFlagValues.LARGE_ARC,
-                Path.SweepFlagValues.POSITIVE_ANGLE, SIX, SEVEN).stroke(ColorKeyword.RED).strokeWidth(FOUR);
+        Path path =
+                new Path().moveTo(0, 1)
+                        .arcTo(THREE, FOUR, FIVE, Path.LargeArcFlagValues.LARGE_ARC,
+                                Path.SweepFlagValues.POSITIVE_ANGLE, SIX, SEVEN)
+                        .stroke(ColorKeyword.RED).strokeWidth(FOUR);
         String actual = path.asString();
         String expected = "<path d=\"M 0 1 A 3 4 5 1 1 6 7\" stroke=\"red\" stroke-width=\"4\"/>";
         assertEquals(expected, actual);
     }
 
     /**
-     * Test verifying that a path describing a red triangle is exported correctly to
-     * a string.
+     * Test verifying that a path describing a red triangle is exported correctly to a string.
      */
     @Test
     void redTriangleIsConvertedCorrectlyToString() {
@@ -79,8 +79,7 @@ public class PathTest {
     }
 
     /**
-     * Test verifying that a path describing a half opaque red triangle is exported
-     * correctly to a string.
+     * Test verifying that a path describing a half opaque red triangle is exported correctly to a string.
      */
     @Test
     void halfOpaqueRedTriangleIsConvertedCorrectlyToString() {
@@ -92,8 +91,7 @@ public class PathTest {
     }
 
     /**
-     * Test verifying that a path describing a red triangle with no fill is exported
-     * correctly to a string.
+     * Test verifying that a path describing a red triangle with no fill is exported correctly to a string.
      */
     @Test
     void redOutlinedTriangleIsConvertedCorrectlyToString() {
@@ -105,14 +103,69 @@ public class PathTest {
     }
 
     /**
-     * Test verifying that a path using hexadecimal integers to specify the fill and
-     * stroke color is exported correctly to a string.
+     * Test verifying that a path using hexadecimal integers to specify the fill and stroke color is exported correctly
+     * to a string.
      */
     @Test
     void pathWithFillAndStrokeSpecifiedAsHexadecimalIntegersIsConvertedCorrectlyToString() {
         Path path = new Path().moveTo(0, 1).lineTo(THREE, FOUR).lineTo(FIVE, SIX).closePath().fill(RED).stroke(BLUE);
         String actual = path.asString();
         String expected = "<path d=\"M 0 1 L 3 4 L 5 6 Z\" fill=\"#FF0000\" stroke=\"#0000FF\"/>";
+        assertEquals(expected, actual);
+    }
+
+    /**
+     * Test verifying that a path with an end marker is exported correctly.
+     */
+    @Test
+    void pathWithEndMarkerShouldBeConvertedCorrectlyToString() {
+        Marker marker = new Marker();
+        marker.setId(1);
+        Path path = new Path().moveTo(0, 1).lineTo(THREE, FOUR).stroke(ColorKeyword.RED).strokeWidth(FOUR)
+                .markerEnd(marker);
+        String actual = path.asString();
+        String expected = "<path d=\"M 0 1 L 3 4\" marker-end=\"url(#marker-1)\" stroke=\"red\" stroke-width=\"4\"/>";
+        assertEquals(expected, actual);
+    }
+
+    /**
+     * Test verifying that a path with an middle marker is exported correctly.
+     */
+    @Test
+    void pathWithMiddleMarkerShouldBeConvertedCorrectlyToString() {
+        Marker marker = new Marker();
+        marker.setId(1);
+        Path path = new Path().moveTo(0, 1).lineTo(THREE, FOUR).stroke(ColorKeyword.RED).strokeWidth(FOUR)
+                .markerMid(marker);
+        String actual = path.asString();
+        String expected = "<path d=\"M 0 1 L 3 4\" marker-mid=\"url(#marker-1)\" stroke=\"red\" stroke-width=\"4\"/>";
+        assertEquals(expected, actual);
+    }
+
+    /**
+     * Test verifying that a path with an start marker is exported correctly.
+     */
+    @Test
+    void pathWithStartMarkerShouldBeConvertedCorrectlyToString() {
+        Marker marker = new Marker();
+        marker.setId(1);
+        Path path = new Path().moveTo(0, 1).lineTo(THREE, FOUR).stroke(ColorKeyword.RED).strokeWidth(FOUR)
+                .markerStart(marker);
+        String actual = path.asString();
+        String expected = "<path d=\"M 0 1 L 3 4\" marker-start=\"url(#marker-1)\" stroke=\"red\" stroke-width=\"4\"/>";
+        assertEquals(expected, actual);
+    }
+
+    /**
+     * Test verifying that a path with a mask is exported correctly.
+     */
+    @Test
+    void pathWithMaskShouldBeConvertedCorrectlyToString() {
+        Mask mask = new Mask();
+        mask.setId(1);
+        Path path = new Path().moveTo(0, 1).lineTo(THREE, FOUR).stroke(ColorKeyword.RED).strokeWidth(FOUR).mask(mask);
+        String actual = path.asString();
+        String expected = "<path d=\"M 0 1 L 3 4\" mask=\"url(#mask-1)\" stroke=\"red\" stroke-width=\"4\"/>";
         assertEquals(expected, actual);
     }
 }
